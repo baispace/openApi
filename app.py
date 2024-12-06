b4c85c8c7f406edfe07151baae60dacd7d8e24fe
Bai He <baihexx@gmail.com>
2024-12-07 00:41:40 +0800

    修改请求参数

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from blueprints.front import bp as front_bp
 
 
 app = Flask(__name__)
-app.config.from_object(config.ProductionConfig)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config.from_object(config.DevelopmentConfig)
 
 db.init_app(app)
 cache.init_app(app)
