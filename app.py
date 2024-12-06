b71e4590e59df7e8f93a7585a3f0a5ed1704374b
Bai He <baihexx@gmail.com>
2024-12-07 01:19:41 +0800

    解决跨域问题，引入cors

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from exts import db, cache
 from blueprints.ai import bp as ai_bp
 from blueprints.busuanzi import bp as busuanzi_bp
 from blueprints.front import bp as front_bp
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -13,6 +14,8 @@ app.config.from_object(config.DevelopmentConfig)
 
 db.init_app(app)
 cache.init_app(app)
+CORS(app)
+
 
 # 注册蓝图
 app.register_blueprint(ai_bp)
