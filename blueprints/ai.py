6a19a44434f1b6e396d682f40d49480e796fcff4
Bai He <baihexx@gmail.com>
2024-12-07 19:19:34 +0800

    完善ai摘要生成接口外部调用异常的处理

--- blueprints/ai.py
+++ blueprints/ai.py
@@ -25,23 +25,22 @@ def get_ai_summary():  # put application's code here
     if model_reply is not None:
         return restful.ok('ok', {'summary': model_reply})
 
-    ali_client = OpenAI(
-        api_key=current_app.config['ALI_API_KEY'],
-        base_url=current_app.config['ALI_API_URL']
-    )
-
-    promote = '你是一个摘要生成工具，请根据我发送的内容生成一段不超过300字的简洁中文摘要，请直接回答这篇文章讲述了什么。摘要应不包含任何链接，并且仅限于描述文章的主要内容，无需提出建议或指出缺失的部分，也不需要提及用户。'
-
-    completion = ali_client.chat.completions.create(
-        model="qwen-turbo",
-        messages=[
-            {'role': 'system', 'content': promote},
-            {'role': 'user', 'content': content}]
-    )
-
-    model_reply = completion.choices[0].message.content
-
-    # 设置缓存，过期时间7
-    cache.set(article_path_key, model_reply, timeout=60 * 60 * 24 * 7)
+    try:
+        ali_client = OpenAI(
+            api_key=current_app.config['ALI_API_KEY'],
+            base_url=current_app.config['ALI_API_URL']
+        )
+        promote = '你是一个摘要生成工具，请根据我发送的内容生成一段不超过300字的简洁中文摘要，请直接回答这篇文章讲述了什么。摘要应不包含任何链接，并且仅限于描述文章的主要内容，无需提出建议或指出缺失的部分，也不需要提及用户。'
+        completion = ali_client.chat.completions.create(
+            model="qwen-turbo",
+            messages=[
+                {'role': 'system', 'content': promote},
+                {'role': 'user', 'content': content}]
+        )
+        model_reply = completion.choices[0].message.content
+        # 设置缓存，过期时间7
+        cache.set(article_path_key, model_reply, timeout=60 * 60 * 24 * 7)
+    except Exception as e:
+        return restful.server_error(e)
 
     return restful.ok('ok', {'summary': model_reply})
