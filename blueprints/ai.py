6ac281ed2b168b718cca7e52b1fb7b6b7fb7cb73
Bai He <baihexx@gmail.com>
2024-12-12 23:54:03 +0800

    修改接口类型为post接口

--- blueprints/ai.py
+++ blueprints/ai.py
@@ -7,35 +7,38 @@ from exts import cache
 bp = Blueprint("ai", __name__, url_prefix="/ai")
 
 
-@bp.route("/summary")
+@bp.route("/summary", methods=["POST"])
 def get_ai_summary():  # put application's code here
     referer = request.referrer
-    if referer is None or 'baispace.cn' not in referer:
-        return restful.params_error('不被允许的站点')
+    if referer is None or "baispace.cn" not in referer:
+        return restful.params_error("不被允许的站点")
 
-    url = request.args.get("url")
-    content = request.args.get("content")
+    data = request.get_json()  # 获取 JSON 数据
+    url = data.get("url")
+    content = data.get("content")
+
     if url is None or content is None:
-        return restful.params_error('参数不能为空')
+        return restful.params_error("参数不能为空")
 
     parsed_url = urlparse(url)
-    article_path_key = 'blog:' + parsed_url.path
+    article_path_key = "blog:" + parsed_url.path
     model_reply = cache.get(article_path_key)
 
     if model_reply is not None:
-        return restful.ok('ok', {'summary': model_reply})
+        return restful.ok("ok", {"summary": model_reply})
 
     try:
         ali_client = OpenAI(
-            api_key=current_app.config['ALI_API_KEY'],
-            base_url=current_app.config['ALI_API_URL']
+            api_key=current_app.config["ALI_API_KEY"],
+            base_url=current_app.config["ALI_API_URL"],
         )
-        promote = '你是一个摘要生成工具，请根据我发送的内容生成一段不超过300字的简洁中文摘要，请直接回答这篇文章讲述了什么。摘要应不包含任何链接，并且仅限于描述文章的主要内容，无需提出建议或指出缺失的部分，也不需要提及用户。'
+        promote = "你是一个摘要生成工具，请根据我发送的内容生成一段不超过300字的简洁中文摘要，请直接回答这篇文章讲述了什么。摘要应不包含任何链接，并且仅限于描述文章的主要内容，无需提出建议或指出缺失的部分，也不需要提及用户。"
         completion = ali_client.chat.completions.create(
             model="qwen-turbo",
             messages=[
-                {'role': 'system', 'content': promote},
-                {'role': 'user', 'content': content}]
+                {"role": "system", "content": promote},
+                {"role": "user", "content": content},
+            ],
         )
         model_reply = completion.choices[0].message.content
         # 设置缓存，过期时间7
@@ -43,4 +46,4 @@ def get_ai_summary():  # put application's code here
     except Exception as e:
         return restful.server_error(e)
 
-    return restful.ok('ok', {'summary': model_reply})
+    return restful.ok("ok", {"summary": model_reply})
