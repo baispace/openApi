d90ea34b24e3911e4cfc3004811fce9cce75d498
baihe <baihe@192.168.0.186>
2024-12-21 01:49:12 +0800

    修改redis配置文件

--- exts.py
+++ exts.py
@@ -9,7 +9,7 @@ cache = Cache()
 
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
-redis_db = int(os.getenv("REDIS_DB", 3))
+redis_db = int(os.getenv("REDIS_DB", 2))
 
 
 redis = Redis(host=redis_host, port=redis_port, db=redis_db, decode_responses=True)
