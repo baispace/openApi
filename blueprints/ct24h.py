96fb2de5eab334f0e2cd99640c7b8612ab27b5bd
BaiHe <baihexx@gmail.com>
2025-01-03 22:04:37 +0800

    增加webHook

--- blueprints/ct24h.py
+++ blueprints/ct24h.py
@@ -30,9 +30,17 @@ def webhook(signature):
         logger.error("Invalid JSON format.")
         return jsonify({"error": "Invalid JSON format"}), 400
 
+    # 提取需要的信息
+    subject = data.get("subject", "")
+    body = data.get("body", "")
+    plain_text = data.get("plain_text", "")
+    # 格式化消息内容
+    message_content = (
+        f"主题: {subject}\n\n内容:\n{body}\n\nプレーンテキスト:\n{plain_text}"
+    )
+
     # 解析 msg_from 字段
     msg_from = data.get("msg_from", [])
-    msg_to = data.get("msg_to", [])
     if msg_from:
         for sender in msg_from:
             # name = sender.get("name")
@@ -43,21 +51,17 @@ def webhook(signature):
                 "receiver@mail.com",
                 "dpxhyme3zexeagn@ct666.online",
                 "admin@ct666.online",
+                "notification@facebookmail.com",
             ):
-                for Toer in msg_from:
-                    to_name = Toer.get("name")
-                    to_address = Toer.get("address")
-                    send_wechat_notification(to_address, to_name)
-
-    logger.info("Received a webhook request successfully.")
+                send_wechat_notification(message_content)
     return jsonify({"status": "OK"}), 200
 
 
-def send_wechat_notification(msg_from, subject):
+def send_wechat_notification(data):
     # 构建企业微信消息
     message = {
         "msgtype": "text",
-        "text": {"content": f"新消息发给: {msg_from}\n标题: {subject}"},
+        "text": data,
     }
 
     # 企业微信 Webhook URL
@@ -65,10 +69,11 @@ def send_wechat_notification(msg_from, subject):
         "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key="
         + current_app.config["WECHAT_WEBHOOK_TOKEN"]
     )
+    # wechat_webhook_url = "https://open.feishu.cn/open-apis/bot/v2/hook/0278130f-be26-4cb5-94e9-621cd00eec36"
 
     # 发送 POST 请求到企业微信 Webhook
     response = requests.post(wechat_webhook_url, json=message)
     if response.status_code != 200:
         logger.error(f"Failed to send notification: {response.text}")
     else:
-        logger.info("Notification sent to WeChat successfully.")
+        logger.info(f"Notification sent to WeChat successfully. {data}")
